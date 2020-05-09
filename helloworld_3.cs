e6a5003d232f5628de6ed7690274c99a851cd170
fulong <menlanshu@hotmail.com>
2020-05-09 10:13:59 +0800

    Test helloeworld 3

---
+++ helloworld_3.cs
@@ -0,0 +1,15 @@
+using system;
+
+namespace helloworld
+{
+	public class helloworld
+	{
+		public static void Main(string[] args)
+		{
+			//add a comment here to test git function
+			//this is from TestBranch
+			Console.WriteLine("Hello World");
+			Console.ReadLine();
+		}
+	}
+}
